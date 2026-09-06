c3e0351dae3e8d1625cfa70e58fe7735293ce440
ChristoJeeson <christo.jeeson@gmail.com>
2026-09-06 15:46:17 +0530

    full c main functions

--- main.c
+++ main.c
@@ -1,11 +1,50 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
-int main()
-{int i=10;
-    int j=90;
-int * const p;
-p=&j;
-*p=90;
-printf("hello world");
+void funct(char *p)
+{
+if(*p)
+{
+    funct(p+1);
+printf("%c",*p);
 
 }
+}
+int main()
+{
+char s[20];
+printf("string");
+scanf("%s",s);
+printf("%s",s);
+printf("\n");
+for(int i=0;s[i];i++)
+{
+    printf("%c",s[i]);
+}
+printf("\n");
+           
+int i=0,j=0;
+
+for(i=0;s[i];i++)
+{
+        for(j=i+1;s[j];j++)
+        {
+            
+            if(s[i]==s[j])
+            {
+                for(int k=j;s[k];k++)
+                {
+                    s[k]=s[k+1];
+                }
+                j--;
+            }
+            
+    }
+
+}
+
+printf("%s",s);
+
+}
+
