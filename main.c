7a210b88c5e22c4b5f75cc568af7c820fb766a7e
ChristoJeeson <christo.jeeson@gmail.com>
2026-09-07 00:25:52 +0530

    full

--- main.c
+++ main.c
@@ -137,6 +137,38 @@ p=q+1;
 string_reverse(p);
 
 }
+
+void binary_number(int num)
+{
+    int pos;
+    for(int pos=31;pos>=0;pos--)
+    {
+      int r=num>>pos&1;
+      printf("%d",r);
+    }
+}
+
+void binary_reverse(int num)
+{
+    int m,n;
+    for(int i=31,j=0;i>j;i--,j++)
+    {
+        m=num>>i&1;
+        n=num>>j&1;
+
+        if(m!=n)
+        {
+            num=num^1<<i;
+            num=num^1<<j;
+        }
+    }
+    for(int pos=31;pos>=0;pos--)
+    {
+        printf("%d",num>>pos&1);
+    }
+}
+
+
 int main()
 {
 
@@ -205,6 +237,122 @@ sentence_word_reorder(sentence);
 printf("\n");
 
 printf("after sentence %s",sentence);
+
+printf("\n");
+
+int num1=256;
+binary_number(num1);
+printf("\n");
+binary_reverse(num1);
+
+//array multiplication
+
+int r1=0,r2=0,c1=0,c2=0;
+printf("\n");
+printf("row1\n");
+scanf("%d",&r1);
+printf("col1\n");
+scanf("%d",&c1);
+printf("row2\n");
+scanf("%d",&r2);
+printf("col2\n");
+scanf("%d",&c2);
+if(c1!=r2)
+{printf("not possible multiplication");
+return 0;
+}
+
+int mat1[r1][c1], mat2[r2][c2], mat3[r1][c2];
+
+
+    printf("matrix1\n");
+    for(int i=0;i<r1;i++)
+    {
+        for(int j=0;j<c1;j++)
+        {
+            scanf("%d",&mat1[i][j]);
+        }
+    }
+
+    for(int i=0;i<r1;i++)
+    {
+        for(int j=0;j<c1;j++)
+        {
+            printf("%d",mat1[i][j]);
+        }
+        printf("\n");
+    }
+
+
+        printf("\nmatrix2\n");
+    for(int i=0;i<r2;i++)
+    {
+        for(int j=0;j<c2;j++)
+        {
+            scanf("%d",&mat2[i][j]);
+        }
+    }
+
+    for(int i=0;i<r2;i++)
+    {
+        for(int j=0;j<c2;j++)
+        {
+            printf("%d",mat2[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for(int i=0;i<r1;i++)
+    {
+        for(int j=0;j<c2;j++)
+        {mat3[i][j]=0;
+            for(int k=0;k<r2;k++)
+            {
+                mat3[i][j]+=mat1[i][k]*mat2[k][j];
+            }
+        }
+    }
+
+    for(int i=0;i<r1;i++)
+    {
+        for(int j=0;j<c2;j++)
+        {
+            printf("%d",mat3[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    printf("malloc\n");
+    int **p;
+    int a1,b1;
+    printf("a1\n");
+    scanf("%d ",&a1);
+    printf("b1\n");
+    scanf("%d",&b1);
+    p=malloc(a1*sizeof(int*));
+
+    for(int i=0;i<a1;i++)
+    {
+        p[i]=malloc(b1*sizeof(int));
+    }
+printf("enter:");
+    for(int i=0;i<a1;i++)
+    {
+        for(int j=0;j<b1;j++)
+        {
+            scanf("%d",&p[i][j]);
+        }
+    }
+
+    for(int i=0;i<a1;i++)
+    {
+        for(int j=0;j<b1;j++)
+        {
+            printf("%d",p[i][j]);
+        }printf("\n");
+    }
 
 }
 
